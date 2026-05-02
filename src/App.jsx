dbc0dda3bfbe15ed628bd836e2a6703ce8710ccd
ebud6 <ebud6805@gmail.com>
2026-05-01 20:23:53 -0500

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import Footer from "./components/Footer/Footer";
+import "./index.css";
 
 function App() {
   return (
     <BrowserRouter basename="/frontend-mentor">
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <div className="app-container">
+
+        <main className="main">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </main>
+
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,37 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer-text">
+        Challenge by{" "}
+        <a
+          href="https://www.frontendmentor.io/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Frontend Mentor
+        </a>
+        . Coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/ethanreynolds119/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Ethan Reynolds
+        </a>
+        . View source code on{" "}
+        <a
+          href="https://github.com/ethanrey19/frontend-mentor"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+        .
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
